7a0948f821301f23414eeeda32f24720dd171f50
lindsfund <fun20001@byui.edu>
2023-10-31 10:08:56 -0400

    Update DeskQuote.cs
    basic Validation beginings

--- MegaDeskSite/MegaDeskSite/Models/DeskQuote.cs
+++ MegaDeskSite/MegaDeskSite/Models/DeskQuote.cs
@@ -13,12 +13,22 @@ namespace MegaDeskSite.Models
         public DateTime Date { get; set; }
 
         [Display(Name ="Name")]
+        [Required]
+        [StringLength(30, MinimumLength = 2)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
         public string CustomerName {  get; set; } = string.Empty;
 
         [Display(Name ="# of Drawers")]
+        [Required]
+        [Range(0,7)]
         public int NumDrawers {  get; set; }
 
+        [Required]
+        [Range(24, 96)]
         public int Width {  get; set; }
+
+        [Required]
+        [Range(12, 48)]
         public int Depth {  get; set; }
 
         public string Material {  get; set; } = string.Empty;
